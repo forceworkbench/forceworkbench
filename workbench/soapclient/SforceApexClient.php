c36394c644aa8774f1db6fbc6852f6ca5a2a74ac
Ryan Brainard <ryan.brainard@gmail.com>
2010-02-21 23:34:21 +0000

    clean up of indendation

--- workbench/soapclient/SforceApexClient.php
+++ workbench/soapclient/SforceApexClient.php
@@ -1,112 +1,108 @@
 <?php
-
 class SforceApexClient {
-  public $sforce;
-  protected $sessionId;
-  protected $location;
+	public $sforce;
+	protected $sessionId;
+	protected $location;
+	protected $namespace = 'http://soap.sforce.com/2006/08/apex';
 
-  protected $namespace = 'http://soap.sforce.com/2006/08/apex';
+	public function __construct($wsdl, $apexServerUrl, $LogCategory, $LogCategoryLevel) {
 
-  public function __construct($wsdl, $apexServerUrl, $LogCategory, $LogCategoryLevel) {
-	
-  	$_SERVER['HTTP_USER_AGENT'] = 'Salesforce/PHPToolkit/1.0';
+		$_SERVER['HTTP_USER_AGENT'] = 'Salesforce/PHPToolkit/1.0';
 
-	$soapClientArray = array();
-	$soapClientArray['trace'] = 1;
-	$soapClientArray['encoding'] = 'utf-8';
+		$soapClientArray = array();
+		$soapClientArray['trace'] = 1;
+		$soapClientArray['encoding'] = 'utf-8';
 
-	//set compression settings
+		//set compression settings
 		if ($_SESSION['config']['enableGzip'] && phpversion() > '5.1.2') {
 			$soapClientArray['compression'] = SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP | 1;
 		}
 
-	//set proxy settings
+		//set proxy settings
 		if ($_SESSION['config']['proxyEnabled'] == true) {
-	  		$proxySettings = array();
-	   		$proxySettings['proxy_host'] = $_SESSION['config']['proxyHost'];
-		    $proxySettings['proxy_port'] = (int)$_SESSION['config']['proxyPort']; // Use an integer, not a string
-	  	    $proxySettings['proxy_login'] = $_SESSION['config']['proxyUsername'];
-	        $proxySettings['proxy_password'] = $_SESSION['config']['proxyPassword'];
-	
-	  	    $soapClientArray = array_merge($soapClientArray, $proxySettings);
+			$proxySettings = array();
+			$proxySettings['proxy_host'] = $_SESSION['config']['proxyHost'];
+			$proxySettings['proxy_port'] = (int)$_SESSION['config']['proxyPort']; // Use an integer, not a string
+			$proxySettings['proxy_login'] = $_SESSION['config']['proxyUsername'];
+			$proxySettings['proxy_password'] = $_SESSION['config']['proxyPassword'];
+
+			$soapClientArray = array_merge($soapClientArray, $proxySettings);
 		}
-    
-    $this->sforce = new SoapClient($wsdl,$soapClientArray);
 
-    //start to set headers
-    	$header_array = array();
-    
-    //set session header
-	    $sessionVar = array('sessionId' => new SoapVar($_SESSION['sessionId'], XSD_STRING));
-	    $headerBody = new SoapVar($sessionVar, SOAP_ENC_OBJECT);
-	    $header_array[] = new SoapHeader($this->namespace, 'SessionHeader', $headerBody, false);
-   
-    //set debugging header
-	    $logInfoComp = array(
-	      'category' => new SoapVar($LogCategory, XSD_STRING),
-	      'level' => new SoapVar($LogCategoryLevel, XSD_STRING)
-	    );
-	    
-	    $logInfoVar = array(
-	    	'categories' => new SoapVar($logInfoComp, SOAP_ENC_OBJECT)
-	    );
-	
-	    $debugBody = new SoapVar($logInfoVar, SOAP_ENC_OBJECT);
-	    
-	    $header_array[] = new SoapHeader($this->namespace, 'DebuggingHeader', $debugBody, false);
- 
-    //set call options header    
-	    if(isset($_SESSION['config']['callOptions_client'])){
-	    	$clientBody = array('client' => new SoapVar($_SESSION['config']['callOptions_client'], XSD_STRING));
-	    	$callOptions_header = new SoapHeader($this->namespace, 'CallOptions', $clientBody, false);
-	    	$header_array[] = $callOptions_header;
-	    } 
-	    
-    //set allowFieldTruncationHeader header    
-	    if(isset($_SESSION['config']['allowFieldTruncationHeader_allowFieldTruncation'])){
-	    	$allowFieldTruncationBody = array('allowFieldTruncation' => new SoapVar($_SESSION['config']['allowFieldTruncationHeader_allowFieldTruncation'], XSD_BOOLEAN));
-	    	$allowFieldTruncationHeader = new SoapHeader($this->namespace, 'AllowFieldTruncationHeader', $allowFieldTruncationBody, false);
-	    	$header_array[] = $allowFieldTruncationHeader;
-	    } 
-    $this->sforce->__setSoapHeaders($header_array);
-    $this->sforce->__setLocation($apexServerUrl);
+		$this->sforce = new SoapClient($wsdl,$soapClientArray);
 
-    return $this->sforce;
-  }
+		//start to set headers
+		$header_array = array();
+
+		//set session header
+		$sessionVar = array('sessionId' => new SoapVar($_SESSION['sessionId'], XSD_STRING));
+		$headerBody = new SoapVar($sessionVar, SOAP_ENC_OBJECT);
+		$header_array[] = new SoapHeader($this->namespace, 'SessionHeader', $headerBody, false);
+
+		//set debugging header
+		$logInfoComp = array(
+				'category' => new SoapVar($LogCategory, XSD_STRING),
+				'level' => new SoapVar($LogCategoryLevel, XSD_STRING)
+		);
+
+		$logInfoVar = array(
+				'categories' => new SoapVar($logInfoComp, SOAP_ENC_OBJECT)
+		);
+
+		$debugBody = new SoapVar($logInfoVar, SOAP_ENC_OBJECT);
+
+		$header_array[] = new SoapHeader($this->namespace, 'DebuggingHeader', $debugBody, false);
+
+		//set call options header    
+		if(isset($_SESSION['config']['callOptions_client'])){
+			$clientBody = array('client' => new SoapVar($_SESSION['config']['callOptions_client'], XSD_STRING));
+			$callOptions_header = new SoapHeader($this->namespace, 'CallOptions', $clientBody, false);
+			$header_array[] = $callOptions_header;
+		} 
+
+		//set allowFieldTruncationHeader header    
+		if(isset($_SESSION['config']['allowFieldTruncationHeader_allowFieldTruncation'])){
+			$allowFieldTruncationBody = array('allowFieldTruncation' => new SoapVar($_SESSION['config']['allowFieldTruncationHeader_allowFieldTruncation'], XSD_BOOLEAN));
+			$allowFieldTruncationHeader = new SoapHeader($this->namespace, 'AllowFieldTruncationHeader', $allowFieldTruncationBody, false);
+			$header_array[] = $allowFieldTruncationHeader;
+		} 
+		$this->sforce->__setSoapHeaders($header_array);
+		$this->sforce->__setLocation($apexServerUrl);
+
+		return $this->sforce;
+	}
 
 
-  public function executeAnonymous($executeAnonymousBlock) {
-  	$executeAnonymousRequest = new stdClass;
-  	$executeAnonymousRequest->String = $executeAnonymousBlock;
-  	$executeAnonymousResult = $this->sforce->__soapCall("executeAnonymous",array($executeAnonymousRequest),null,null,$outputHeaders);
-  	
-  	$executeAnonymousResultWithDebugLog = new stdClass;
-  	$executeAnonymousResultWithDebugLog->executeAnonymousResult = $executeAnonymousResult->result;
-  	foreach($outputHeaders as $outputHeader){
-  		$executeAnonymousResultWithDebugLog->debugLog .= $outputHeader->debugLog;
-  	}
-  	return $executeAnonymousResultWithDebugLog;
-  }
- 
+	public function executeAnonymous($executeAnonymousBlock) {
+		$executeAnonymousRequest = new stdClass;
+		$executeAnonymousRequest->String = $executeAnonymousBlock;
+		$executeAnonymousResult = $this->sforce->__soapCall("executeAnonymous",array($executeAnonymousRequest),null,null,$outputHeaders);
 
-  public function getLastRequest() {
-    return $this->sforce->__getLastRequest();
-  }
+		$executeAnonymousResultWithDebugLog = new stdClass;
+		$executeAnonymousResultWithDebugLog->executeAnonymousResult = $executeAnonymousResult->result;
+		foreach($outputHeaders as $outputHeader){
+			$executeAnonymousResultWithDebugLog->debugLog .= $outputHeader->debugLog;
+		}
+		return $executeAnonymousResultWithDebugLog;
+	}
 
-  public function getLastRequestHeaders() {
-    return $this->sforce->__getLastRequestHeaders();
-  }
 
-  public function getLastResponse() {
-    return $this->sforce->__getLastResponse();
-  }
+	public function getLastRequest() {
+		return $this->sforce->__getLastRequest();
+	}
 
-  public function getLastResponseHeaders() {
-    return $this->sforce->__getLastResponseHeaders();
-  }
+	public function getLastRequestHeaders() {
+		return $this->sforce->__getLastRequestHeaders();
+	}
+
+	public function getLastResponse() {
+		return $this->sforce->__getLastResponse();
+	}
+
+	public function getLastResponseHeaders() {
+		return $this->sforce->__getLastResponseHeaders();
+	}
 
 
 }
-
-
 ?>
--- workbench/soapclient/SforcePartnerClient.php
+++ workbench/soapclient/SforcePartnerClient.php
@@ -38,170 +38,170 @@ require_once ('SforceEmail.php');
  * @package SalesforceSoapClient
  */
 class SforcePartnerClient extends SforceBaseClient {
-  const PARTNER_NAMESPACE = 'urn:partner.soap.sforce.com';
+	const PARTNER_NAMESPACE = 'urn:partner.soap.sforce.com';
 
-  function SforcePartnerClient() {
-    $this->namespace = self::PARTNER_NAMESPACE;
-  }
+	function SforcePartnerClient() {
+		$this->namespace = self::PARTNER_NAMESPACE;
+	}
 
-  /**
-   * Adds one or more new individual objects to your organization's data.
-   * @param array $sObjects    Array of one or more sObjects (up to 200) to create.
-   * @param AssignmentRuleHeader $assignment_header is optional.  Defaults to NULL
-   * @param MruHeader $mru_header is optional.  Defaults to NULL
-   * @return SaveResult
-   */
-  public function create($sObjects) {
-    $arg = new stdClass;
-    foreach ($sObjects as $sObject) {
-      if (isset ($sObject->fields)) {
-        $sObject->any = $this->_convertToAny($sObject->fields);
-      }
-    }
-    $arg->sObjects = $sObjects;
-    return parent::_create($arg);
-  }
+	/**
+	 * Adds one or more new individual objects to your organization's data.
+	 * @param array $sObjects    Array of one or more sObjects (up to 200) to create.
+	 * @param AssignmentRuleHeader $assignment_header is optional.  Defaults to NULL
+	 * @param MruHeader $mru_header is optional.  Defaults to NULL
+	 * @return SaveResult
+	 */
+	public function create($sObjects) {
+		$arg = new stdClass;
+		foreach ($sObjects as $sObject) {
+			if (isset ($sObject->fields)) {
+				$sObject->any = $this->_convertToAny($sObject->fields);
+			}
+		}
+		$arg->sObjects = $sObjects;
+		return parent::_create($arg);
+	}
 
-  /**
-   * Merge records
-   *
-   * @param stdclass $mergeRequest
-   * @param String $type
-   * @return unknown
-   */
-  public function merge($mergeRequest) {
-    if (isset($mergeRequest->masterRecord)) {
-      if (isset ($mergeRequest->masterRecord->fields)) {
-        $mergeRequest->masterRecord->any = $this->_convertToAny($mergeRequest->masterRecord->fields);
-      }
-      //return parent::merge($mergeRequest, $type);
-      $arg = new stdClass;
-      $arg->request = $mergeRequest;
-      return $this->_merge($arg);
-    }
-  }
+	/**
+	 * Merge records
+	 *
+	 * @param stdclass $mergeRequest
+	 * @param String $type
+	 * @return unknown
+	 */
+	public function merge($mergeRequest) {
+		if (isset($mergeRequest->masterRecord)) {
+			if (isset ($mergeRequest->masterRecord->fields)) {
+				$mergeRequest->masterRecord->any = $this->_convertToAny($mergeRequest->masterRecord->fields);
+			}
+			//return parent::merge($mergeRequest, $type);
+			$arg = new stdClass;
+			$arg->request = $mergeRequest;
+			return $this->_merge($arg);
+		}
+	}
 
-  public function sendSingleEmail($request) {
-    if (is_array($request)) {
-      $messages = array();
-      foreach ($request as $r) {
-        $email = new SoapVar($r, SOAP_ENC_OBJECT, 'SingleEmailMessage', $this->namespace);
-        array_push($messages, $email);
-      }
-      $arg = new stdClass;
-      $arg->messages = $messages;
-      return parent::_sendEmail($arg);
-    } else {
-      $backtrace = debug_backtrace();
-      die('Please pass in array to this function:  '.$backtrace[0]['function']);
-    }
-  }
+	public function sendSingleEmail($request) {
+		if (is_array($request)) {
+			$messages = array();
+			foreach ($request as $r) {
+				$email = new SoapVar($r, SOAP_ENC_OBJECT, 'SingleEmailMessage', $this->namespace);
+				array_push($messages, $email);
+			}
+			$arg = new stdClass;
+			$arg->messages = $messages;
+			return parent::_sendEmail($arg);
+		} else {
+			$backtrace = debug_backtrace();
+			die('Please pass in array to this function:  '.$backtrace[0]['function']);
+		}
+	}
 
-  public function sendMassEmail($request) {
-    if (is_array($request)) {
-      $messages = array();
-      foreach ($request as $r) {
-        $email = new SoapVar($r, SOAP_ENC_OBJECT, 'MassEmailMessage', $this->namespace);
-        array_push($messages, $email);
-      }
-      $arg = new stdClass;
-      $arg->messages = $messages;
-      return parent::_sendEmail($arg);
-    } else {
-      $backtrace = debug_backtrace();
-      die('Please pass in array to this function:  '.$backtrace[0]['function']);
-    }
-  }
+	public function sendMassEmail($request) {
+		if (is_array($request)) {
+			$messages = array();
+			foreach ($request as $r) {
+				$email = new SoapVar($r, SOAP_ENC_OBJECT, 'MassEmailMessage', $this->namespace);
+				array_push($messages, $email);
+			}
+			$arg = new stdClass;
+			$arg->messages = $messages;
+			return parent::_sendEmail($arg);
+		} else {
+			$backtrace = debug_backtrace();
+			die('Please pass in array to this function:  '.$backtrace[0]['function']);
+		}
+	}
 
-  /**
-   * Updates one or more new individual objects to your organization's data.
-   * @param array sObjects    Array of sObjects
-   * @param AssignmentRuleHeader $assignment_header is optional.  Defaults to NULL
-   * @param MruHeader $mru_header is optional.  Defaults to NULL
-   * @return UpdateResult
-   */
-  public function update($sObjects) {
-    $arg = new stdClass;
-    foreach ($sObjects as $sObject) {
-      if (isset ($sObject->fields)) {
-        $sObject->any = $this->_convertToAny($sObject->fields);
-      }
-    }
-    $arg->sObjects = $sObjects;
-    return parent::_update($arg);
-  }
+	/**
+	 * Updates one or more new individual objects to your organization's data.
+	 * @param array sObjects    Array of sObjects
+	 * @param AssignmentRuleHeader $assignment_header is optional.  Defaults to NULL
+	 * @param MruHeader $mru_header is optional.  Defaults to NULL
+	 * @return UpdateResult
+	 */
+	public function update($sObjects) {
+		$arg = new stdClass;
+		foreach ($sObjects as $sObject) {
+			if (isset ($sObject->fields)) {
+				$sObject->any = $this->_convertToAny($sObject->fields);
+			}
+		}
+		$arg->sObjects = $sObjects;
+		return parent::_update($arg);
+	}
 
-  /**
-   * Creates new objects and updates existing objects; uses a custom field to
-   * determine the presence of existing objects. In most cases, we recommend
-   * that you use upsert instead of create because upsert is idempotent.
-   * Available in the API version 7.0 and later.
-   *
-   * @param string $ext_Id        External Id
-   * @param array  $sObjects  Array of sObjects
-   * @return UpsertResult
-   */
-  public function upsert($ext_Id, $sObjects) {
-    //		$this->_setSessionHeader();
-    $arg = new stdClass;
-    $arg->externalIDFieldName = new SoapVar($ext_Id, XSD_STRING, 'string', 'http://www.w3.org/2001/XMLSchema');
-    foreach ($sObjects as $sObject) {
-      if (isset ($sObject->fields)) {
-        $sObject->any = $this->_convertToAny($sObject->fields);
-      }
-    }
-    $arg->sObjects = $sObjects;
-    return parent::_upsert($arg);
-  }
+	/**
+	 * Creates new objects and updates existing objects; uses a custom field to
+	 * determine the presence of existing objects. In most cases, we recommend
+	 * that you use upsert instead of create because upsert is idempotent.
+	 * Available in the API version 7.0 and later.
+	 *
+	 * @param string $ext_Id        External Id
+	 * @param array  $sObjects  Array of sObjects
+	 * @return UpsertResult
+	 */
+	public function upsert($ext_Id, $sObjects) {
+		//		$this->_setSessionHeader();
+		$arg = new stdClass;
+		$arg->externalIDFieldName = new SoapVar($ext_Id, XSD_STRING, 'string', 'http://www.w3.org/2001/XMLSchema');
+		foreach ($sObjects as $sObject) {
+			if (isset ($sObject->fields)) {
+				$sObject->any = $this->_convertToAny($sObject->fields);
+			}
+		}
+		$arg->sObjects = $sObjects;
+		return parent::_upsert($arg);
+	}
 }
 
 class QueryResult {
-  public $queryLocator;
-  public $done;
-  public $records;
-  public $size;
+	public $queryLocator;
+	public $done;
+	public $records;
+	public $size;
 
-  public function __construct($response) {
-    $this->queryLocator = $response->queryLocator;
-    $this->done = $response->done;
-    $this->size = $response->size;
+	public function __construct($response) {
+		$this->queryLocator = $response->queryLocator;
+		$this->done = $response->done;
+		$this->size = $response->size;
 
-    $this->records = array();
+		$this->records = array();
 
-    if (isset($response->records)) {
-      if (is_array($response->records)) {
-        foreach ($response->records as $record) {
-          $sobject = new SObject($record);
-          array_push($this->records, $sobject);
-        };
-      } else {
-        $sobject = new SObject($response->records);
-        array_push($this->records, $sobject);
-      }
-    }
-  }
+		if (isset($response->records)) {
+			if (is_array($response->records)) {
+				foreach ($response->records as $record) {
+					$sobject = new SObject($record);
+					array_push($this->records, $sobject);
+				};
+			} else {
+				$sobject = new SObject($response->records);
+				array_push($this->records, $sobject);
+			}
+		}
+	}
 }
 
 
 class SearchResult {
-  public $searchRecords;
+	public $searchRecords;
 
-  public function __construct($response) {
+	public function __construct($response) {
 
-    $this->searchRecords = array();
+		$this->searchRecords = array();
 
-    if (isset($response->searchRecords)) {
-      if (is_array($response->searchRecords)) {
-        foreach ($response->searchRecords as $record) {
-          $sobject = new SObject($record);
-          array_push($this->searchRecords, $sobject);
-        };
-      } else {
-        $sobject = new SObject($response->searchRecords);
-        array_push($this->searchRecords, $sobject);
-      }
-    }
-  }
+		if (isset($response->searchRecords)) {
+			if (is_array($response->searchRecords)) {
+				foreach ($response->searchRecords as $record) {
+					$sobject = new SObject($record);
+					array_push($this->searchRecords, $sobject);
+				};
+			} else {
+				$sobject = new SObject($response->searchRecords);
+				array_push($this->searchRecords, $sobject);
+			}
+		}
+	}
 }
 
 /**
@@ -210,66 +210,66 @@ class SearchResult {
  * @package SalesforceSoapClient
  */
 class SObject {
-  public $type;
-  public $fields;
-  public $fieldsToNull;
-  //  public $sobject;
+	public $type;
+	public $fields;
+	public $fieldsToNull;
+	//  public $sobject;
 
-  public function __construct($response=NULL) {
-    if (isset($response)) {
-      if (isset($response->Id)) $this->Id = $response->Id[0];
-      if (isset($response->type)) $this->type = $response->type;
-      if (isset($response->fieldsToNull)) $this->fieldsToNull = $response->fieldsToNull;
-      if (isset($response->any)) {
-        try {
-          //$this->fields = $this->convertFields($response->any);
-          // If ANY is an object, instantiate another SObject
-          if ($response->any instanceof stdClass) {
-            if ($this->isSObject($response->any)) {
-              $anArray = array();
-              $sobject = new SObject($response->any);
-              array_push($anArray, $sobject);
-              $this->sobjects = $anArray;
-            } else {
-              // this is for parent to child relationships
-              $this->queryResult = new QueryResult($response->any);
-            }
+	public function __construct($response=NULL) {
+		if (isset($response)) {
+			if (isset($response->Id)) $this->Id = $response->Id[0];
+			if (isset($response->type)) $this->type = $response->type;
+			if (isset($response->fieldsToNull)) $this->fieldsToNull = $response->fieldsToNull;
+			if (isset($response->any)) {
+				try {
+					//$this->fields = $this->convertFields($response->any);
+					// If ANY is an object, instantiate another SObject
+					if ($response->any instanceof stdClass) {
+						if ($this->isSObject($response->any)) {
+							$anArray = array();
+							$sobject = new SObject($response->any);
+							array_push($anArray, $sobject);
+							$this->sobjects = $anArray;
+						} else {
+							// this is for parent to child relationships
+							$this->queryResult = new QueryResult($response->any);
+						}
 
-          } else {
-            // If ANY is an array
-            if (is_array($response->any)) {
-              // Loop through each and perform some action.
-              $anArray = array();
-              foreach ($response->any as $item) {
-                if ($item instanceof stdClass) {
-                  if ($this->isSObject($item)) {
-                    $sobject = new SObject($item);
-                    array_push($anArray, $sobject);
-                  } else {
-                    // this is for parent to child relationships
-                    //$this->queryResult = new QueryResult($item);
-                    if (!isset($this->queryResult)) {
-                      $this->queryResult = array();
-                    }
-                    array_push($this->queryResult, new QueryResult($item));
-                  }
-                } else {
-                  //$this->fields = $this->convertFields($item);
-                  if (!isset($fieldsToConvert)) {
-                    $fieldsToConvert = $item;
-                  } else {
-                    $fieldsToConvert .= $item;
-                  }
-                }
-                if (isset($fieldsToConvert)) {
-                  $this->fields = $this->convertFields($fieldsToConvert);
-                }
-              }
-              if (sizeof($anArray) > 0) {
-                $this->sobjects = $anArray;
-              }
+					} else {
+						// If ANY is an array
+						if (is_array($response->any)) {
+							// Loop through each and perform some action.
+							$anArray = array();
+							foreach ($response->any as $item) {
+								if ($item instanceof stdClass) {
+									if ($this->isSObject($item)) {
+										$sobject = new SObject($item);
+										array_push($anArray, $sobject);
+									} else {
+										// this is for parent to child relationships
+										//$this->queryResult = new QueryResult($item);
+										if (!isset($this->queryResult)) {
+											$this->queryResult = array();
+										}
+										array_push($this->queryResult, new QueryResult($item));
+									}
+								} else {
+									//$this->fields = $this->convertFields($item);
+									if (!isset($fieldsToConvert)) {
+										$fieldsToConvert = $item;
+									} else {
+										$fieldsToConvert .= $item;
+									}
+								}
+								if (isset($fieldsToConvert)) {
+									$this->fields = $this->convertFields($fieldsToConvert);
+								}
+							}
+							if (sizeof($anArray) > 0) {
+								$this->sobjects = $anArray;
+							}
 
-              /*
+							/*
                $this->fields = $this->convertFields($response->any[0]);
                if (isset($response->any[1]->records)) {
                $anArray = array();
@@ -291,42 +291,42 @@ class SObject {
                array_push($anArray, $sobject);
                $this->sobjects = $anArray;
                }
-               */
-            } else {
-              $this->fields = $this->convertFields($response->any);
-            }
-          }
-        } catch (Exception $e) {
-          var_dump($e);
-        }
-      }
-    }
-  }
+							 */
+						} else {
+							$this->fields = $this->convertFields($response->any);
+						}
+					}
+				} catch (Exception $e) {
+					var_dump($e);
+				}
+			}
+		}
+	}
 
-  /**
-   * Parse the "any" string from an sObject.  First strip out the sf: and then
-   * enclose string with <Object></Object>.  Load the string using
-   * simplexml_load_string and return an array that can be traversed.
-   */
-  function convertFields($any) {
-    $new_string = ereg_replace('sf:', '', $any);
-    $new_string = '<Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'.$new_string.'</Object>';
-    $xml = simplexml_load_string($new_string);
-    return $xml;
-  }
+	/**
+	 * Parse the "any" string from an sObject.  First strip out the sf: and then
+	 * enclose string with <Object></Object>.  Load the string using
+	 * simplexml_load_string and return an array that can be traversed.
+	 */
+	function convertFields($any) {
+		$new_string = ereg_replace('sf:', '', $any);
+		$new_string = '<Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'.$new_string.'</Object>';
+		$xml = simplexml_load_string($new_string);
+		return $xml;
+	}
 
-  /*
-   * If the stdClass has a done, we know it is a QueryResult
-   */
-  function isQueryResult($param) {
-    return isset($param->done);
-  }
+	/*
+	 * If the stdClass has a done, we know it is a QueryResult
+	 */
+	function isQueryResult($param) {
+		return isset($param->done);
+	}
 
-  /*
-   * If the stdClass has a type, we know it is an SObject
-   */
-  function isSObject($param) {
-    return isset($param->type);
-  }
+	/*
+	 * If the stdClass has a type, we know it is an SObject
+	 */
+	function isSObject($param) {
+		return isset($param->type);
+	}
 }
 ?>
