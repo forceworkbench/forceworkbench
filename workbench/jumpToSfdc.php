629279deb3f53513f7dd552e6e1b2f467658ad3b
Ryan Brainard <ryan.brainard@gmail.com>
2011-02-17 10:14:13 +0000

    Issue 380: 	Jump to SFDC Failing First Time -- fixed issue with content door and -api urls -- need to do more testing through for all situations

--- workbench/jumpToSfdc.php
+++ workbench/jumpToSfdc.php
@@ -6,13 +6,31 @@ if (!isset($_GET['startUrl'])) {
     throw new Exception("startUrl param not set");
 }
 
-preg_match("@(https?://.*)/services@", $_SESSION['location'], $instUIDomain);
+preg_match("@((https?://)((.*)-api)?(.*))/services@", $_SESSION['location'], $sfdcApiHost);
+
+// [1] => https://na4-api.salesforce.com
+// [2] => https://
+// [3] => na4-api
+// [4] => na4
+// [5] => .salesforce.com
+
+if ($sfdcApiHost[3] != null) {
+    //special cases
+    switch ($sfdcApiHost[4]) {
+        case "na0": $sfdcApiHost[4] = "ssl"; break;
+        case "ap0": $sfdcApiHost[4] = "ap"; break;
+        case "eu0": $sfdcApiHost[4] = "emea"; break;
+    }
+    $sfdcUiHost = $sfdcApiHost[2] . $sfdcApiHost[4] . $sfdcApiHost[5];
+} else {
+    $sfdcUiHost = $sfdcApiHost[1];
+}
 
 if (getConfig("useSfdcFrontdoor") == 'ALWAYS' || (getConfig("useSfdcFrontdoor") == 'AUTO' && !$_SESSION['sfdcUiSidLikelySet'])) {
     $_SESSION['sfdcUiSidLikelySet'] = true;
-    $jumpUrl = "$instUIDomain[1]/secur/frontdoor.jsp?sid=". $_SESSION['sessionId'] . "&retURL=%2F" . $_GET['startUrl'];
+    $jumpUrl = "$sfdcUiHost/secur/frontdoor.jsp?sid=". $_SESSION['sessionId'] . "&retURL=%2F" . $_GET['startUrl'];
 } else {
-    $jumpUrl = "$instUIDomain[1]/" . $_GET['startUrl'];
+    $jumpUrl = "$sfdcUiHost/" . $_GET['startUrl'];
 }
 
 header("Location: " . $jumpUrl);
