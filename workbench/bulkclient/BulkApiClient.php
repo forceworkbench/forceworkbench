877c9e5409cdb52a0778a5add0ef41b9daece361
Ryan Brainard <ryan.brainard@gmail.com>
2011-03-07 04:43:08 +0000

    Issue 399:  	 Streamed Bulk API Results Blank
     *This needs testing on Mac

--- workbench/bulkclient/BulkApiClient.php
+++ workbench/bulkclient/BulkApiClient.php
@@ -337,11 +337,15 @@ class BulkApiClient {
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeaders);
         if($isPost) curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);                                //TODO: use ca-bundle instead
         if($this->compressionEnabled) curl_setopt($ch, CURLOPT_ENCODING, "gzip");   //TODO: add  outbound compression support
-        if (isset($toFile)) curl_setopt($ch, CURLOPT_FILE, $toFile);
+
+        if (isset($toFile)) {
+            curl_setopt($ch, CURLOPT_FILE, $toFile);
+        } else {
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        }
 
         $this->log("REQUEST \n POST: $isPost \n URL: $url \n HTTP HEADERS: \n" . print_r($httpHeaders, true) . " DATA:\n " . htmlentities($data));
 
