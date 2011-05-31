eafa3204404fc1d8dcd50fade1e9184019732ae9
Ryan Brainard <ryan.brainard@gmail.com>
2011-05-31 01:51:48 +0000

    401 of wbctx isn't established for proxy

--- workbench/sfdcProxy.php
+++ workbench/sfdcProxy.php
@@ -3,6 +3,12 @@ require_once "context/WorkbenchContext.php";
 require_once "cometd-resourses/PhpReverseProxy.php";
 require_once "session.php";
 
+if (!WorkbenchContext::isEstablished()) {
+    header('HTTP/1.0 401 Unauthorized');
+    echo "SFDC Proxy only available if Workbench Context has been established.";
+    exit;
+}
+
 $host = WorkbenchContext::get()->getHost();
 $sessionId = WorkbenchContext::get()->getSessionId();
 session_write_close();
