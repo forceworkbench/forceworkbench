13ddbe1d7222c2463ade145ad36250ccfa68e54d
Ryan Brainard <ryan.brainard@gmail.com>
2011-05-31 05:31:20 +0000

    simplification of proxy and local testing

--- workbench/sfdcProxy.php
+++ workbench/sfdcProxy.php
@@ -1,21 +1,27 @@
 <?php
 require_once "context/WorkbenchContext.php";
 require_once "streaming/PhpReverseProxy.php";
-require_once "session.php";
 
-if (!WorkbenchContext::isEstablished()) {
-    header('HTTP/1.0 401 Unauthorized');
-    echo "SFDC Proxy only available if Workbench Context has been established.";
-    exit;
-}
-
-$host = WorkbenchContext::get()->getHost();
-$sessionId = WorkbenchContext::get()->getSessionId();
-session_write_close();
+//require_once "session.php";
+//
+//if (!WorkbenchContext::isEstablished()) {
+//    header('HTTP/1.0 401 Unauthorized');
+//    echo "SFDC Proxy only available if Workbench Context has been established.";
+//    exit;
+//}
+//
+//$host = WorkbenchContext::get()->getHost();
+//$sessionId = WorkbenchContext::get()->getSessionId();
+//$_COOKIE['sid'] = $sessionId;
+//session_write_close();
 
 $proxy = new PhpReverseProxy();
-$proxy->host = $host;
-$_COOKIE['sid'] = $sessionId;
+//$proxy->host = $host;
+$proxy->host = "localhost";
+$proxy->port = "8080";
+$proxy->forward_path = "/dojo-jetty7-primer";
+
+
 $proxy->connect();
 $proxy->output();
 ?>
--- workbench/streaming/PhpReverseProxy.php
+++ workbench/streaming/PhpReverseProxy.php
@@ -1,139 +1,107 @@
 <?php
 
-class PhpReverseProxy{
-	public $port,$host,$forward_path,$content,$content_type,$user_agent,
-		$XFF,$request_method,$IMS,$cacheTime,$cookie;
+class PhpReverseProxy {
+    public $port, $host, $forward_path, $content, $content_type, $user_agent,
+    $XFF, $request_method, $cookie;
 
-    private $http_code,$lastModified,$version,$resultHeader;
+    private $http_code, $version, $resultHeader;
 
-    function __construct(){
-		$this->version="PHP Reverse Proxy (PRP) 1.0";
-		$this->port="";
-		$this->host="127.0.0.1";
-        $this->forward_path="";
-		$this->content="";
-		$this->path="";
-		$this->content_type="";
-		$this->user_agent="";
-		$this->http_code="";
-		$this->XFF="";
-		$this->request_method="GET";
-		$this->IMS=false;	// If-Modified-Since
-		$this->cacheTime=72000;
-		$this->lastModified=gmdate("D, d M Y H:i:s",time()-72000)." GMT";
-		$this->cookie="";
-	}
+    function __construct() {
+        $this->version = "PHP Reverse Proxy (PRP) 1.0";
+        $this->port = "";
+        $this->host = "";
+        $this->forward_path = "";
+        $this->content = "";
+        $this->path = "";
+        $this->content_type = "";
+        $this->user_agent = "";
+        $this->http_code = "";
+        $this->XFF = "";
+        $this->request_method = "GET";
+        $this->cookie = "";
+    }
 
-	function translateURL($serverName) {
-		$this->path = $this->forward_path . str_replace(dirname($_SERVER['PHP_SELF']), "", $_SERVER['REQUEST_URI']);
-		if($_SERVER['QUERY_STRING']=="")
-			return $this->translateServer($serverName).$this->path;
-		else
-		return $this->translateServer($serverName).$this->path."?".$_SERVER['QUERY_STRING'];
-	}
+    function translateURL($serverName) {
+        $this->path = $this->forward_path . str_replace(dirname($_SERVER['PHP_SELF']), "", $_SERVER['REQUEST_URI']);
+        $server = $this->translateServer($serverName);
+        $queryString = ($_SERVER['QUERY_STRING'] == "")
+                ? ""
+                : "?" . $_SERVER['QUERY_STRING'];
+
+        return $server . $this->path . $queryString;
+    }
 
     function translateServer($serverName) {
-		$s = empty($_SERVER["HTTPS"]) ? ''
-			: ($_SERVER["HTTPS"] == "on") ? "s"
-			: "";
-		$protocol = $this->left(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s;
-		if($this->port=="") 
-			return $protocol."://".$serverName;
-		else
-			return $protocol."://".$serverName.":".$this->port;
-	}
+        $protocol = "http" .
+                    ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on")
+                        ? "s"
+                        : "");
 
-    function left($s1, $s2) {
-		return substr($s1, 0, strpos($s1, $s2));
-	}
+        $port = ($this->port == "") ? "" : ":" . $this->port;
 
-    function preConnect(){
-		$this->user_agent = $_SERVER['HTTP_USER_AGENT'];
-		$this->request_method = $_SERVER['REQUEST_METHOD'];
-		$tempCookie = "";
-		foreach ($_COOKIE as $cookieName => $cookieValue) {
+        return $protocol . "://" . $serverName . $port;
+    }
+
+    function preConnect() {
+        $this->user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $this->request_method = $_SERVER['REQUEST_METHOD'];
+        $tempCookie = "";
+        foreach ($_COOKIE as $cookieName => $cookieValue) {
             if ($cookieName == "PHPSESSID") continue;
             if ($cookieName == "XDEBUG_SESSION") continue;
-			$tempCookie = $tempCookie." $cookieName = $cookieValue;";
-		}
-		$this->cookie = $tempCookie;
-		if(empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-			$this->XFF = $_SERVER['REMOTE_ADDR'];
-		} else {
-			$this->XFF = $_SERVER['HTTP_X_FORWARDED_FOR'].", ".$_SERVER['REMOTE_ADDR'];
-		}
-	}
+            $tempCookie = $tempCookie . " $cookieName = $cookieValue;";
+        }
+        $this->cookie = $tempCookie;
+        if (empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $this->XFF = $_SERVER['REMOTE_ADDR'];
+        } else {
+            $this->XFF = $_SERVER['HTTP_X_FORWARDED_FOR'] . ", " . $_SERVER['REMOTE_ADDR'];
+        }
+    }
 
-    function connect(){
-		if(empty($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
-			$this->preConnect();
-			$ch=curl_init();
-			if($this->request_method=="POST"){
-				curl_setopt($ch, CURLOPT_POST,1);
-				curl_setopt($ch, CURLOPT_POSTFIELDS,file_get_contents("php://input"));
-			}
-			curl_setopt($ch,CURLOPT_URL,$this->translateURL($this->host));
+    function connect() {
+        $this->preConnect();
+        $ch = curl_init();
+        if ($this->request_method == "POST") {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents("php://input"));
+        }
+        curl_setopt($ch, CURLOPT_URL, $this->translateURL($this->host));
 
-            $header = array();
-            foreach (getallheaders() as $key => $value) {
-                if (in_array($key, array("Content-Type", "Accept"))) {
-                    $headers[] = "$key: $value";
-                }
+        $headers = array();
+        foreach (getallheaders() as $key => $value) {
+            if (in_array($key, array("Content-Type", "Accept"))) {
+                $headers[] = "$key: $value";
             }
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-			if($this->cookie!=""){
-				curl_setopt($ch,CURLOPT_COOKIE,$this->cookie);
-			}
-			curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true); 
-			curl_setopt($ch,CURLOPT_AUTOREFERER,true); 
-			curl_setopt($ch,CURLOPT_HEADER,true);
-			curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-			$output = curl_exec($ch);
-			$info	= curl_getinfo( $ch );
-            curl_close($ch);
-			$this->postConnect($info,$output);
-		}else {
-			$this->lastModified=$_SERVER['HTTP_IF_MODIFIED_SINCE'];
-			$this->IMS=true;
-		}
-	}
+        if ($this->cookie != "") {
+            curl_setopt($ch, CURLOPT_COOKIE, $this->cookie);
+        }
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+        $this->postConnect($info, $output);
+    }
 
-    function postConnect($info,$output){
-		$this->content_type = $info["content_type"];
-		$this->http_code = $info['http_code'];
-		if (!empty($info['last_modified'])){
-			$this->lastModified=$info['last_modified'];
-		}
-		$this->resultHeader = substr($output,0,$info['header_size']);
-		$content = substr($output,$info['header_size']);
-		if ($this->http_code == '200'){
-			$this->content=$content;
-		} 
-	}
+    function postConnect($info, $output) {
+        $this->content_type = $info["content_type"];
+        $this->http_code = $info['http_code'];
+        $this->resultHeader = substr($output, 0, $info['header_size']);
+        $this->content = substr($output, $info['header_size']);
+    }
 
-    function output(){
-		$currentTimeString=gmdate("D, d M Y H:i:s",time());
-		$expiredTime=gmdate("D, d M Y H:i:s",(time()+$this->cacheTime));
-		if ($this->IMS){
-			header("HTTP/1.1 304 Not Modified");
-			header("Date: Wed, $currentTimeString GMT");
-			header("Last-Modified: $this->lastModified");
-			header("Server: $this->version");
-		} else {
-			header("HTTP/1.1 200 OK"); //todo: shoulnd't this be set to what the server actually responded???
-			header("Date: Wed, $currentTimeString GMT");
-			header("Content-Type: ".$this->content_type);
-			header("Last-Modified: $this->lastModified");
-			header("Cache-Control: max-age=$this->cacheTime");
-			header("Expires: $expiredTime GMT");
-			header("Server: $this->version");
-			preg_match("/Set-Cookie:[^\n]*/i",$this->resultHeader,$result);
-			foreach($result as $i=>$value){
-				header($result[$i]);
-			}
-			echo($this->content);
-		}
-	}
+    function output() {
+        foreach (explode("\n",$this->resultHeader) as $h) {
+            header($h);
+        }
+        echo $this->content;
+    }
 }
+
 ?>
