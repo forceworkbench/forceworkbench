9156acc893b1b9d308da32825f3cf1c7b59cd116
Ryan Brainard <ryan.brainard@gmail.com>
2011-06-18 04:05:19 +0000

    add assertion messages to describe tests

--- workbench-test/java/src/forceworkbench/tests/DescribeTests.java
+++ workbench-test/java/src/forceworkbench/tests/DescribeTests.java
@@ -10,8 +10,8 @@ public class DescribeTests extends WorkbenchSeleneseBaseTest {
 	public void testDescribeAllObjects()throws Exception{
 		selenium.open("describe.php");			
 		selenium.waitForPageToLoad(WAIT_TIMEOUT);
-		assertTrue(selenium.isTextPresent("Choose an object to describe:"));
-		assertFalse(selenium.isTextPresent("Expand All | Collapse All"));
+		assertTrue("Instructions should appear before selecting a type to describe", selenium.isTextPresent("Choose an object to describe:"));
+		assertFalse("Tree controls should appear before selecting a type to describe", selenium.isTextPresent("Expand All | Collapse All"));
 
 		for(String type : selenium.getSelectOptions("default_object")){			
 			if(type.equalsIgnoreCase("")) continue;
@@ -19,11 +19,11 @@ public class DescribeTests extends WorkbenchSeleneseBaseTest {
 			selenium.select("default_object", "label=" + type);
 			selenium.waitForPageToLoad(WAIT_TIMEOUT);
 
-            assertTrue(selenium.isTextPresent("Choose an object to describe:"));
-			assertTrue(selenium.isTextPresent("Expand All | Collapse All"));
-			assertTrue(selenium.isTextPresent("Attributes"));
-			assertTrue(selenium.isTextPresent("Fields ("));
-			assertEquals(type, selenium.getSelectedValue("default_object"));
+            assertTrue("Instructions should appear for [" + type + "]", selenium.isTextPresent("Choose an object to describe:"));
+			assertTrue("Tree controls should appear for [" + type + "]", selenium.isTextPresent("Expand All | Collapse All"));
+			assertTrue("Attributes should appear for [" + type + "]", selenium.isTextPresent("Attributes"));
+			assertTrue("Fields should appear for [" + type + "]", selenium.isTextPresent("Fields ("));
+			assertEquals("Default object should be set to [" + type + "]", type, selenium.getSelectedValue("default_object"));
 			assertNoPhpErrors();
 		}
 	}
