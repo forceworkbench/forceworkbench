21e6baa8c3c176b8bd886880d471bb75a0aa6bcc
Ryan Brainard <ryan.brainard@gmail.com>
2012-02-12 15:44:13 -0800

    Update other version number and copyright occurances to 24.0 and 2012 respectively

--- workbench/bulkclient/BulkApiClient.php
+++ workbench/bulkclient/BulkApiClient.php
@@ -3,11 +3,11 @@ require_once 'JobInfo.php';
 require_once 'BatchInfo.php';
 
 /**
- * PHP BULK API CLIENT 23.0.0
+ * PHP BULK API CLIENT 24.0.0
  * @author Ryan Brainard
  *
  * BulkApiClient.php
- * Main client for interacting with the REST-based Force.com Bulk API 23.0
+ * Main client for interacting with the REST-based Force.com Bulk API 24.0
  * to asynchronously insert, update, and upsert data to Salesforce.
  * Requires PHP cURL library to be installed.
  *
@@ -21,7 +21,7 @@ require_once 'BatchInfo.php';
  * * Feedback & Discussion
  *   http://groups.google.com/group/forceworkbench
  *
- * Copyright (c) 2011, salesforce.com, inc.
+ * Copyright (c) 2012, salesforce.com, inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
@@ -63,7 +63,7 @@ class BulkApiClient {
     private $endpoint;
     private $sessionId;
     private $proxySettings;
-    private $userAgent = "PHP-BulkApiClient/23.0.0";
+    private $userAgent = "PHP-BulkApiClient/24.0.0";
     private $compressionEnabled = true;
     private $includeSessionCookie = false;
     private $logs;
--- workbench/bulkclient/JobInfo.php
+++ workbench/bulkclient/JobInfo.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * PHP BULK API CLIENT 23.0.0
+ * PHP BULK API CLIENT 24.0.0
  * @author Ryan Brainard
  *
  * JobInfo.php
- * Represents a Force.com Bulk API 23.0 JobInfo object.
+ * Represents a Force.com Bulk API 24.0 JobInfo object.
  *
  * For reference, see:
  * http://www.salesforce.com/us/developer/docs/api_asynch/Content/asynch_api_reference_jobinfo.htm
