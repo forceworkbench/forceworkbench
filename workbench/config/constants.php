92f0cfa554842ae7df2faa339093e74534540af6
Ryan Brainard <ryan.brainard@gmail.com>
2012-08-08 16:39:30 -0700

    mark as "25.0.1 Alpha"

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "25.0.0 Beta 1";
+$GLOBALS["WORKBENCH_VERSION"] = "25.0.1 Alpha";
 
 class Page {
     public $title;
