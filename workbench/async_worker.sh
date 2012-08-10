5fd5e478ca2c208ab9088a5a80c587e629e5669a
Ryan Brainard <ryan.brainard@gmail.com>
2012-08-10 14:00:28 -0700

    only touch and log if forceworkbench__logFile__default is set

--- workbench/async_worker.sh
+++ workbench/async_worker.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-touch ${forceworkbench__logFile__default}
-tail -F ${forceworkbench__logFile__default} &
+if [ -n "${forceworkbench__logFile__default}" ]; then
+    touch "${forceworkbench__logFile__default}"
+    tail -F "${forceworkbench__logFile__default}" &
+fi
 
 while true; do
     $1 async_worker.php
