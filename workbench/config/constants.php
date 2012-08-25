b1985c4e4b913e73fb353fa318b91810ce60e062
Ryan Brainard <ryan.brainard@gmail.com>
2012-08-24 18:07:12 -0700

    25.0.1 Beta 10: signed requests and 26.0 basic support

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "25.0.1 Beta 9";
+$GLOBALS["WORKBENCH_VERSION"] = "25.0.1 Beta 10";
 
 class Page {
     public $title;
