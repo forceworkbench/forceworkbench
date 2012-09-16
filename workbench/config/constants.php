dfa2856db83182a1397c11e834d91425fefe5614
Ryan Brainard <ryan.brainard@gmail.com>
2012-09-16 15:39:09 -0700

    25.0.3

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "25.0.2";
+$GLOBALS["WORKBENCH_VERSION"] = "25.0.3";
 
 class Page {
     public $title;
