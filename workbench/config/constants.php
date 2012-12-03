7882d40f7b1346fc7d611b0c66857bfd9f0668b8
Ryan Brainard <ryan.brainard@gmail.com>
2012-12-03 15:38:40 -0800

    log queryCurrentRecord failures

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "25.0.5";
+$GLOBALS["WORKBENCH_VERSION"] = "25.0.6";
 
 class Page {
     public $title;
--- workbench/put.php
+++ workbench/put.php
@@ -367,18 +367,13 @@ function queryCurrentRecord($describeSObjectResult, $id) {
             " FROM " . $describeSObjectResult->name .
             " WHERE Id = '" . $id . "'";
 
-    // TODO: run multiple queries and remove this stupid restriction
-    if (strlen($soql) >= 10000) {
-        throw new WorkbenchHandledException("This feature is currently unavailable because " . $describeSObjectResult->name . " contains too many fields, " .
-                                            "This limitation should be fixed in a future version of Workbench.");
-    }
-
     try {
         $queryResponse = WorkbenchContext::get()->getPartnerConnection()->queryAll($soql);
         if ($queryResponse->size == 1) {
             return new SObject($queryResponse->records[0]);
         }
     } catch (Exception $e) {
+        workbenchLog(LOG_DEBUG, "queryCurrentRecord failed", $e->getMessage());
         return null;
     }
 
