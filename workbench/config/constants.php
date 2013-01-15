b98241f4e93d36d2ba6e37d4c3c119b2eeb7aa36
Ryan Brainard <ryan.brainard@gmail.com>
2013-01-15 09:39:01 -0800

    Fix de to be German @ 26.0.1

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "26.0.0";
+$GLOBALS["WORKBENCH_VERSION"] = "26.0.1";
 
 class Page {
     public $title;
