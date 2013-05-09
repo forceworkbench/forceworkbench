3e86495d8fde8949dccb5afc0d6323731da00369
Ryan Brainard <ryan.brainard@gmail.com>
2013-05-09 13:02:42 -0700

    Fix regex for REST API version detection @27.0.9

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "27.0.8";
+$GLOBALS["WORKBENCH_VERSION"] = "27.0.9";
 
 class Page {
     public $title;
