e7c6507c3903ebddb813a472ff6c59400fd9656a
Ryan Brainard <ryan.brainard@gmail.com>
2013-07-09 12:35:18 -0700

    set 28.0 as default version @ 28.0.0

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "27.0.10";
+$GLOBALS["WORKBENCH_VERSION"] = "28.0.0";
 
 class Page {
     public $title;
