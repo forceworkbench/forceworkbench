82e6b98040021ebe2b9c40a4d1d79eb14cb42470
Ryan Brainard <ryan.brainard@gmail.com>
2014-02-19 23:37:12 -0800

    Add preliminary API 30.0 support

--- workbench/config/defaults.php
+++ workbench/config/defaults.php
@@ -275,6 +275,7 @@ $config["header_LoginOptions"] = array(
     );
 
     $GLOBALS['API_VERSIONS'] = array(
+        "30.0" => "30.0",
         "29.0" => "29.0",
         "28.0" => "28.0",
         "27.0" => "27.0",
