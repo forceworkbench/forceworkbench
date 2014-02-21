d800d4dbb2039c55f8f371d503947b6569a2ac10
Ryan Brainard <ryan.brainard@gmail.com>
2014-02-20 19:41:49 -0800

    "29.0.4 Beta 2"

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "29.0.4 Beta 1";
+$GLOBALS["WORKBENCH_VERSION"] = "29.0.4 Beta 2";
 
 class Page {
     public $title;
