57bc04734d4a2064890f31a80cbc4391219b7017
Ryan Brainard <brainard@heroku.com>
2015-07-29 20:21:23 -0700

    Auto-configure $REDIS_URL

--- .profile.d/redis.sh
+++ .profile.d/redis.sh
@@ -3,3 +3,9 @@ if [ -n "$REDISTOGO_URL" ]; then
   export forceworkbench__redisUrl__default="$REDISTOGO_URL"
   export forceworkbench__sessionStore__default="$REDISTOGO_URL"
 fi
+
+if [ -n "$REDIS_URL" ]; then
+  echo "Configuring Redis"
+  export forceworkbench__redisUrl__default="$REDIS_URL"
+  export forceworkbench__sessionStore__default="$REDIS_URL"
+fi
