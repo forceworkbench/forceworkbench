9862582b18e30675cffcb8ee9b99aa05e8d508cc
Ryan Brainard <brainard@heroku.com>
2016-05-30 11:38:20 +0900

    36.0.6

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "36.0.5";
+$GLOBALS["WORKBENCH_VERSION"] = "36.0.6";
 
 class Page {
     public $title;
