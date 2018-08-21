c8e757db369fa2bb812877a78fd2fae1b461cb72
Nathan Totten <nathan@ntotten.com>
2018-08-21 10:20:29 -0700

    Update constants.php

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "44.0.0";
+$GLOBALS["WORKBENCH_VERSION"] = "43.0.0";
 
 class Page {
     public $title;
