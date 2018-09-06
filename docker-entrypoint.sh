51c8c74e31a6b6a209d3c741689a256fdd6175b8
Jonathan <jwidjaja@salesforce.com>
2018-09-06 13:00:01 -0400

    Add entrypoint that gets host IP on Linux hosts

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function fix_linux_internal_host() {
+  DOCKER_INTERNAL_HOST="host.docker.internal"
+
+  if ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
+    DOCKER_INTERNAL_IP=`/sbin/ip route | awk '/default/ { print $3 }' | awk '!seen[$0]++'`
+    echo -e "$DOCKER_INTERNAL_IP\t$DOCKER_INTERNAL_HOST" | tee -a /etc/hosts > /dev/null
+    echo "Added $DOCKER_INTERNAL_HOST to hosts /etc/hosts"
+  fi
+}
+
+fix_linux_internal_host
+vendor/bin/heroku-php-apache2 -F fpm_custom_local.conf -i local_php.ini workbench
