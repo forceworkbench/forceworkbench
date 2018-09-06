2171fa9120efc3b800a91a758304b3844d92c5cd
Jonathan <jwidjaja@salesforce.com>
2018-09-06 16:07:41 -0400

    Run linux workaround on linux hosts only

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,9 @@
 
 function fix_linux_internal_host() {
   DOCKER_INTERNAL_HOST="host.docker.internal"
+  ping -q -c1 $DOCKER_INTERNAL_HOST > /dev/null 2>&1
 
-  if ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
+  if [ $? -ne 0 ] && ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
     DOCKER_INTERNAL_IP=`/sbin/ip route | awk '/default/ { print $3 }' | awk '!seen[$0]++'`
     echo -e "$DOCKER_INTERNAL_IP\t$DOCKER_INTERNAL_HOST" | tee -a /etc/hosts > /dev/null
     echo "Added $DOCKER_INTERNAL_HOST to hosts /etc/hosts"
