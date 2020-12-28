70f94617fcfcf7e1609842b921b7b34c90ab5e33
Georges Nguyen <georges.nguyen@salesforce.com>
2020-12-28 11:24:58 -0800

    Add version 51.0.

--- workbench/config/defaults.php
+++ workbench/config/defaults.php
@@ -276,6 +276,7 @@ $config["header_LoginOptions"] = array(
     );
 
     $GLOBALS['API_VERSIONS'] = array(
+        "51.0" => "51.0",
         "50.0" => "50.0",
         "49.0" => "49.0",
         "48.0" => "48.0",
@@ -324,7 +325,7 @@ $config["header_LoginOptions"] = array(
     $config["defaultApiVersion"]  = array(
         "label" => "Default API Version",
         "description" => "Default API version to be used for login. This setting does not affect the API version of the current session. Recommended to choose latest version. Some features may act unexpectedly when using older versions.",
-        "default" => "49.0",
+        "default" => "50.0",
         "overrideable" => true,
         "dataType" => "picklist",
         "valuesToLabels" => $GLOBALS['API_VERSIONS']
