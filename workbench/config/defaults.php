182212e31c08d9e6cc9b03f50302ab13a52d2bb3
GermanMaly <106086821+GermanMaly@users.noreply.github.com>
2022-07-26 19:46:06 -0300

    Upgrade workbench public. Version to 55 (#850)
    Co-authored-by: gmaly <gmaly@gmaly-ltmd75u.internal.salesforce.com>

--- workbench/config/defaults.php
+++ workbench/config/defaults.php
@@ -276,6 +276,7 @@ $config["header_LoginOptions"] = array(
     );
 
     $GLOBALS['API_VERSIONS'] = array(
+		"55.0" => "55.0",
         "54.0" => "54.0",
         "53.0" => "53.0",
         "52.0" => "52.0",
@@ -328,7 +329,7 @@ $config["header_LoginOptions"] = array(
     $config["defaultApiVersion"]  = array(
         "label" => "Default API Version",
         "description" => "Default API version to be used for login. This setting does not affect the API version of the current session. Recommended to choose latest version. Some features may act unexpectedly when using older versions.",
-        "default" => "53.0",
+        "default" => "54.0",
         "overrideable" => true,
         "dataType" => "picklist",
         "valuesToLabels" => $GLOBALS['API_VERSIONS']
