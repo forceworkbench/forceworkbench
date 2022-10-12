76373546a2c2eeaba91447551763188a9e008569
BenGearset <113354590+BenGearset@users.noreply.github.com>
2022-10-12 10:04:23 +0100

    Bump workbench version

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "55.0.0";
+$GLOBALS["WORKBENCH_VERSION"] = "56.0.0";
 
 class Page {
     public $title;
