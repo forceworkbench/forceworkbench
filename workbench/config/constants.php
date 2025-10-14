a803a33ce91bb1a22837ae2a3fc239c7fc856277
Serdar-Ozcan <serdar.ozcan@gearset.com>
2025-10-14 16:11:10 +0100

    include api versions 63.0-65.0

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -7,7 +7,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$GLOBALS["WORKBENCH_VERSION"] = "62.0.0";
+$GLOBALS["WORKBENCH_VERSION"] = "65.0.0";
 
 class Page {
     public $title;
